c12a62ceeb34e09f48a57864bd2584009fc9ee84
Elchin <elchin.kerimov.99@gmail.com>
2020-11-03 12:32:49 +0300

    Update explicit_second_order.m

--- 7sem/computational_math/explicit_second_order.m
+++ 7sem/computational_math/explicit_second_order.m
@@ -1,37 +1,39 @@
 %a = 0;
 %b = 1;
-T = 4;
+T = 1;
 format shortG;
 
-for n = [100]
-    n
-    xs = linspace(0, 1, n+1);
+max_errors_explicit = [];
+taus = logspace(-4, -1, 4);
+
+for tau=taus
+    tau
+    h = sqrt(2.1 * tau);
+    xs = 0:h:1;
     prev_t = 0;
-    tau = 1 / (4 * n .^ 2);
     U_prev = U_0(xs).';
     
     res = U_prev;
     real = U_prev.';
     for t = tau:tau:T-tau
-        U_prev = explicit_algo(xs, t, tau, @U_a, @U_b, U_prev, @F);
+        U_prev = explicit_algo(xs, t, tau, U_prev, @F);
         res = cat(2, res, U_prev);
         real = cat(1, real, U(xs, t));
     end
-    figure;
-    contour(abs(res.' - real), 'ShowText', 'on');
-    max(max(abs(res.' - real)))
+%      figure;
+%      contour(abs(res.' - real), 'ShowText', 'on');
+    max_errors_explicit = [max_errors_explicit max(max(abs(res.' - real)))];
 end
+
+figure;
+loglog(taus, max_errors_explicit, '-o', taus, 4 * taus, '-o');
+grid on;
+legend('Явный', 'h^2');
+xlabel('tau');
+ylabel('Максимальная ошибка');
 
 function res = U(x, t)
     res = exp(x) .* sin(x .* t);
-end
-
-function res_a = U_a(t)
-    res_a = U(0, t);
-end
-
-function res_b = U_b(t)
-    res_b = U(1, t);
 end
 
 function res_0 = U_0(x)
@@ -42,14 +44,13 @@ function res_f = F(x, t)
     res_f = exp(x) .* (cos(x .* t) .* (x - 2 * t) - sin(x .* t) .* (1 - t .^ 2));
 end
 
-
-function next_time = explicit_algo(xs, next_t, tau, U_a, U_b, U_prev, F)
+function next_time = explicit_algo(xs, next_t, tau, U_prev, F)
     h = xs(2) - xs(1);
     x_len = numel(xs) - 2;
     
     next_time = zeros(numel(xs), 1);
-    next_time(1) = U_a(next_t);
-    next_time(numel(xs)) = U_b(next_t);
+    next_time(1) = U(xs(1), next_t);
+    next_time(numel(xs)) = U(xs(numel(xs)), next_t);
     
     for i=2:x_len+1
         diff = (U_prev(i+1) - 2 * U_prev(i) + U_prev(i-1)) / (h .^ 2);
